d54e2a2f7f6647ab272d63d5ce8f6ac9e9327dcd
pranjal dhakal <pranjal750@gmail.com>
2018-07-22 21:59:16 +0545

    Implementation of a shallow NN

---
+++ Shallow_NN/Shallow_NN.py
@@ -0,0 +1,10 @@
+#in this task, I will create a data set (like a spiral dataset), and perform classification using 1-hidden layered neural network.
+#if I can find a dataset in internet, I will use that as well.
+
+#aim -> use scikit learn to load dataset.
+#    -> perform classification using self built 1-layered NN.
+#    -> perform visualization using matplotlib and scikit.
+
+
+
+
