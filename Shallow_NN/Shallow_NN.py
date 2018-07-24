fa94ea6b683380d1a0a468cf5611094d505524d9
pranjal1 <pranjal750@gmail.com>
2018-07-24 22:49:04 +0545

    Completed the Shallow NN

--- Shallow_NN/Shallow_NN.py
+++ Shallow_NN/Shallow_NN.py
@@ -7,17 +7,40 @@
 
 
 
-#Starting with the assumption that training and testing data are already provided in these numpy arrays.
+import numpy as np
+from sklearn.datasets.samples_generator import make_blobs,make_moons,make_circles,make_s_curve
+from matplotlib import pyplot
+from pandas import DataFrame
+import matplotlib.pyplot as plt
 
-#x_train.shape = (nx,m)
-#y_train.shape = (1,m)
 
-#x_test.shape = (p,nx)
-#y_train.shape = (1,p)
 
-#task tomorrow
-#select a relevant dataset and perform the actual training and see the accuracies
-#make a way to save the trained parameters and create an option to either train from scratch or make predictions using the saved parameters
+
+# generate 2d classification dataset
+#X, y = make_s_curve(n_samples=100, noise=0.1) #not applicable
+X, y = make_blobs(n_samples=100, centers=2, n_features=2)
+#X, y = make_moons(n_samples=5000, noise=0.1)
+#X, y = make_circles(n_samples=10000, noise=0.05)
+X_train = X.T
+#print X_train.shape
+Y_train = y.reshape(1,y.shape[0])
+print X_train.shape,Y_train.shape
+#print Y_train.shape
+
+
+# scatter plot, dots colored by class value
+df = DataFrame(dict(x=X[:,0], y=X[:,1], label=y))
+colors = {0:'red', 1:'blue', 2:'green'}
+fig, ax = pyplot.subplots()
+grouped = df.groupby('label')
+for key, group in grouped:
+    group.plot(ax=ax, kind='scatter', x='x', y='y', label=key, color=colors[key])
+pyplot.show()
+
+
+
+
+
 
 
 #initializing the model layer parameters:
@@ -31,9 +54,9 @@ def layer_sizes(train_shape,output_shape): #(nx,m),(1,m)
 #initialize the weights and biases for all the layers
 
 def initialize_parameters(n_x,n_h,n_y):
-	W1 = np.random.randn(n_h,n_x) * 0.01
+	W1 = np.random.randn(n_h,n_x) * 1
 	b1 = np.zeros((n_h,1))
-	W2 = np.random.randn(n_y,n_h) * 0.01
+	W2 = np.random.randn(n_y,n_h) * 1
 	b2 = np.zeros((n_y,1))
 	
 	parameters = {"W1":W1,"b1":b1,"W2":W2,"b2":b2}
@@ -50,7 +73,7 @@ def forward_propagation(X,parameters): #this function will be called in a loop
 	Z1 = np.dot(W1,X) + b1
 	A1 = np.tanh(Z1)
 	Z2 = np.dot(W2,A1) + b2
-	A2 = 1/(1+np.exp(-Z2))
+	A2 = float(1)/(1+np.exp(-Z2))
 
 	cache = {"Z1":Z1,"A1":A1,"Z2":Z2,"A2":A2}
 	return A2,cache
@@ -61,7 +84,7 @@ def compute_cost(A2,Y): #this function will be called once every 1000s like iter
 	m = Y.shape[1]
 	
 	log_part = -(np.multiply(Y,np.log(A2)) + np.multiply((1-Y),np.log(1-A2)))
-	cost = (1/m) * np.sum(log_part)#this cost will be matrix form
+	cost = (float(1)/m) * np.sum(log_part)#this cost will be matrix form
 	#to remove the matrix form
 	cost = np.squeeze(cost)
 	
@@ -79,12 +102,12 @@ def back_propagation(parameters,cache,X,Y):
 	A1 = cache["A1"]
 
 	dZ2 = A2-Y #since sigmoid activation in the last layer
-	db2 = (1/m)*np.sum(dZ2)
-	dW2 = (1/m) * np.dot(dZ2,A1.T)
+	db2 = (float(1)/m)*np.sum(dZ2)
+	dW2 = (float(1)/m) * np.dot(dZ2,A1.T)
 
 	dZ1 = np.multiply(np.dot(W2.T,dZ2),(1-np.power(A1,2)))
-	dW1 = (1/m)* np.dot(dZ1,X.T)
-	db1 = (1/m)* np.sum(dZ1,axis=1,keepdims=True)
+	dW1 = (float(1)/m)* np.dot(dZ1,X.T)
+	db1 = (float(1)/m)* np.sum(dZ1,axis=1,keepdims=True)
 
 	grads = {"dW1":dW1,"dW2":dW2,"db1":db1,"db2":db2}
 	return grads
@@ -97,15 +120,15 @@ def update_parameters(parameters,grads,learning_rate = 1.2): #learning_rate is t
 	b1 = parameters["b1"]
 	b2 = parameters["b2"]
 
-	dW1 = cache["dW1"]
-	dW2 = cache["dW2"]
-	db1 = cache["db1"]
-	db2 = cache["db2"]
+	dW1 = grads["dW1"]
+	dW2 = grads["dW2"]
+	db1 = grads["db1"]
+	db2 = grads["db2"]
 
 	W1 = W1 - learning_rate * dW1
-	W2 = W1 - learning_rate * dW2
-	b1 = W1 - learning_rate * db1
-	b2 = W1 - learning_rate * db2
+	W2 = W2 - learning_rate * dW2
+	b1 = b1 - learning_rate * db1
+	b2 = b2 - learning_rate * db2
 	
 	parameters = {"W1":W1,"b1":b1,"W2":W2,"b2":b2}
 	return parameters
@@ -113,14 +136,17 @@ def update_parameters(parameters,grads,learning_rate = 1.2): #learning_rate is t
 
 
 def shallow_nn_model(X,Y,n_h,num_iterations = 10000,print_cost=False):
+	#print X.shape,Y.shape
 	np.random.seed(3) #for consistency
 	#define layer parameters,n_h is already set by user
+	train_shape = X.shape
+	output_shape = Y.shape
 	n_x,n_y=layer_sizes(train_shape,output_shape)
 	#initialize weights and biases for the first time
 	parameters=initialize_parameters(n_x,n_h,n_y)
-	
+	#print parameters
 	#perform training	
-	for i in range(num_iterations):
+	for i in range(num_iterations+1):
 		#one forward pass
 		A2,cache = forward_propagation(X,parameters)
 		#compute cost
@@ -130,7 +156,7 @@ def shallow_nn_model(X,Y,n_h,num_iterations = 10000,print_cost=False):
 		#perform update on parameters
 		parameters = update_parameters(parameters,grads)
 	
-		if(i%1000 == 0 and print_cost):
+		if(i%100 == 0 and print_cost):
 			print "cost after "+str(i)+" iterations = "+str(cost)
 
 	
@@ -143,13 +169,35 @@ def predict(X,parameters):
 	return predictions
 
 
+def color_set(Z):
+	cols = []
+	for x in Z:
+		if x == 0:
+			cols.append('Pink')
+		if x == 1:
+			cols.append('Green')
+	return cols
+
+def plot_decision_boundary(X,Y,parameters):
+	# plot the resulting classifier
+	h = 0.02
+	x_min, x_max = X[0, :].min() - 1, X[0, :].max() + 1
+	y_min, y_max = X[1, :].min() - 1, X[1, :].max() + 1
+	xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+		             np.arange(y_min, y_max, h))
+	Z = ((np.dot(parameters["W2"],np.dot(parameters["W1"],np.c_[xx.ravel(), yy.ravel()].T) + parameters["b1"]) + parameters["b2"])>0.5)*1
+	fig = plt.figure()
+	cols = color_set(Z[0,:])
+	plt.scatter(xx, yy,c=cols,s=40, cmap=plt.cm.Spectral)
+	plt.scatter(X_train[0,:], X_train[1,:],c=Y_train[0,:],s=40, cmap=plt.cm.Spectral)
+	plt.xlim(xx.min(), xx.max())
+	plt.ylim(yy.min(), yy.max())
+	fig.savefig('boundary_4_neurons.png')
+
+
 
 #running the training 
-parameters = shallow_nn_model(X_train,Y_train,n_h = 5,num_iterations = 10000,print_cost=True)
-
-# Plot the decision boundary
-plot_decision_boundary(lambda x: predict(parameters, x.T), X, Y)
-plt.title("Decision Boundary for hidden layer size " + str(5))
+parameters = shallow_nn_model(X_train,Y_train,n_h = 4,num_iterations = 5000,print_cost=True)
 
 
 #make predictions
@@ -157,10 +205,17 @@ predictions = predict(X_train,parameters)
 # Print training accuracy
 print ('Training Accuracy: %d' % float((np.dot(Y_train,predictions.T) + np.dot(1-Y_train,1-predictions.T))/float(Y_train.size)*100) + '%')
 
+
+plot_decision_boundary(X_train,Y_train,parameters)
+
+
+
+
+'''
 #after this use a testing sample
 
 #make predictions
 predictions = predict(X_test,parameters)
 # Print training accuracy
 print ('Testing Accuracy: %d' % float((np.dot(Y_test,predictions.T) + np.dot(1-Y_test,1-predictions.T))/float(Y_test.size)*100) + '%')
-
+'''
